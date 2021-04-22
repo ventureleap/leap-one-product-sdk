aae29f0db6769dc17b1e0e031f83a68487f97bdb
GitHub Action <action@github.com>
2021-04-22 12:22:54 +0000

    Product Service commit hash: 8293030

--- git_push.sh
+++ git_push.sh
@@ -18,7 +18,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Product Service commit hash: 2495cc0"
+    release_note="Product Service commit hash: 8293030"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
